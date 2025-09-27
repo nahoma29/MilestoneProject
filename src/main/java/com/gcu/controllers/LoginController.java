914c3c9848f2964816326d1faa118514f101fae0
amanuelhaile <amanuelhaile@localhost>
2025-09-26 22:52:06 -0700

    login commit

--- src/main/java/com/gcu/controllers/LoginController.java
+++ src/main/java/com/gcu/controllers/LoginController.java
@@ -1,19 +1,37 @@
 package com.gcu.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gcu.models.LoginModel;
+
+import jakarta.validation.Valid;
+
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 	
 	@GetMapping("/")
-	@ResponseBody
-	public String loginController() {
+	public String display(Model model) {
 		
-		return "Milestone Project";
+		model.addAttribute("title", "Login Form");
+		model.addAttribute("loginModel", new LoginModel());
+		return "login";
+	}
+	@PostMapping("/processLogin")
+	public String doLogin(@Valid LoginModel loginModel, BindingResult bindngResult, Model model) {
+		if(bindngResult.hasErrors()) {
+			model.addAttribute("title", "Login Form");
+			return "login";
+		}
+		
+	    model.addAttribute("loginModel", loginModel);
+		return "loginResults.html";
 	}
 	// this is the new change made 
 	
--- src/main/java/com/gcu/models/LoginModel.java
+++ src/main/java/com/gcu/models/LoginModel.java
@@ -1,16 +1,23 @@
 package com.gcu.models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class LoginModel {
-	
+	@NotNull(message="Username is a required field.")
+	@Size(min=3, max=15, message="Username must be between 3 and 15 characters.")
 	private String username;
+	@NotNull(message="Password is a required field.")
+	@Size(min=8, max=15, message="Password must be between 8 and 15 characters.")
 	private String password;
 	
 	public LoginModel(String username, String password) {
-		
 		this.username = username;
 		this.password = password;
 	}
-
+	public LoginModel() {
+		
+	}
 	public String getUsername() {
 		return username;
 	}
